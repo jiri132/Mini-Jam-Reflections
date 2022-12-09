324b3101b9a38e71c36a9b944e17577120dbcbc2
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-09 13:48:15 +0100

    Impleemented: MovmeentManager

---
+++ Assets/Scripts/Player/Movement/ControllerControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerControls : MovementManager
+{
+    public override Vector2 Direction()
+    {
+        float x = Input.GetAxis("Horizontal"),
+              y = Input.GetAxis("Vertical");
+
+        return new Vector2(x, y);
+    }
+
+    public override bool OnGround()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Player/Movement/MovementManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class MovementManager
+{
+    /// <summary>
+    /// Direction of the player
+    /// </summary>
+    /// <returns>Gives back the value of direction</returns>
+    public abstract Vector2 Direction();
+
+    /// <summary>
+    /// Is the player on the ground?
+    /// </summary>
+    /// <returns>returns ture if it is on the ground</returns>
+    public abstract bool OnGround();
+
+    /// <summary>
+    /// Speed of the player
+    /// </summary>
+    public float Speed = 10f;
+}
