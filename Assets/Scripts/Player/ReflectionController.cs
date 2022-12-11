23fc96ee4c8c59eee6aeece9d42fd262c5adff64
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-11 01:32:25 +0100

    Added: Reflection Script

---
+++ Assets/Scripts/Player/ReflectionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionController : MonoBehaviour
+{
+    [SerializeField] private PlayerController _player;
+
+    private void Awake() => _player = FindObjectOfType<PlayerController>();
+    
+
+
+}
