767f324248807fd433f6129414642e9c909996fb
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-11 02:15:29 +0100

    Added: Spawning of reflection players

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,5 +6,28 @@ using Librarys.Singleton;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    [SerializeField] private GameObject _reflectionPrefab;
+    [SerializeField] private Mirror[] _mirrors;
+
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 0) { return; }
+
+        _mirrors = FindObjectsOfType<Mirror>();
+
+        foreach (Mirror mirror in _mirrors)
+        {
+            if (mirror._mirrorDirection == Mirror.mirrorDirection.Horizontal)
+            {
+                 Instantiate(_reflectionPrefab).GetComponent<ReflectionController>().reflectionType = ReflectionController.ReflectionType.Vertical; 
+            }else
+            {
+                Instantiate(_reflectionPrefab).GetComponent<ReflectionController>().reflectionType = ReflectionController.ReflectionType.Horizontal;
+            }
+        }
+    }
+
+        
 }
