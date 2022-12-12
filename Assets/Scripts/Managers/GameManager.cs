864db88c27eb33e1161f32a996e2430bdc46bc92
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-12-12 01:47:05 +0100

    Functional: Music

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,14 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Librarys.Singleton;
 
 
 public class GameManager : Singleton<GameManager>
 {
     public GameObject _reflectionPrefab;
-    public List<Mirror> _castedMirrors;
-    public List<Vector2> Fields = new List<Vector2>();
     public List<ReflectionController> _reflections = new List<ReflectionController>();
+
+    [SerializeField] private AudioClip MainMenuTheme;
+    [SerializeField] private AudioClip PuzzleTheme;
+    [SerializeField] private AudioClip SFX_Falling;
+
+    public AudioSource[] audioSources;
+
+    public void PlayDeathSound()
+    {
+        audioSources[1].clip = SFX_Falling;
+        audioSources[1].Play();
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            audioSources[0].clip = MainMenuTheme;
+            audioSources[0].Play();
+        }
+        else
+        {
+            audioSources[0].clip = PuzzleTheme;
+            audioSources[0].Play();
+        }
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+        {
+            audioSources[0].clip = MainMenuTheme;
+            audioSources[0].Play();
+        }
+        else
+        {
+            audioSources[0].clip = PuzzleTheme;
+            audioSources[0].Play();
+        }
+    }
 
 }
